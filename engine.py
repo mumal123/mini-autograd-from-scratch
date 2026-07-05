51885d34d7ca950666f2cda919a08307087a4b1f
mumal123 <mumalswami@gmail.com>
2026-07-05 16:24:42 +0530

    Implemented Value class and autograd logic

---
+++ engine.py
@@ -0,0 +1,91 @@
+import math
+
+class Value:
+    def __init__(self,data,_children=(),_op=''):
+        self.data=data
+        self._prev=set(_children)
+        self.op=_op
+        self.grad=0.0
+        self._backward=lambda:None
+
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    
+    def __add__(self,other):
+        other=other if isinstance(other,Value) else Value(other)
+        out=Value(self.data+other.data,_children=(self,other),_op='+')
+        def _backward():
+            self.grad+=1.0*out.grad
+            other.grad+=1.0*out.grad
+        out._backward=_backward
+        return out
+    
+    def __radd__(self, other): #fallback
+        return self + other
+    
+    def __mul__(self,other):
+        other=other if isinstance(other,Value) else Value(other)
+        out=Value(self.data*other.data,_children=(self,other),_op='*')
+        def _backward():
+            self.grad+=other.data*out.grad # used accumulation because of multiple paths
+            other.grad+=self.data*out.grad
+        out._backward=_backward
+        return out
+    
+    def __rmul__(self,other): #fallback
+        return self*other
+    
+    def __pow__(self,other):
+        assert isinstance(other,(int,float))
+        out=Value(self.data**other,_children=(self,),_op=f'**{other}')
+        def _backward():
+            self.grad+=other*(self.data**(other-1))*out.grad
+        out._backward=_backward
+        return out
+    
+    def __sub__(self,other):
+        other=other if isinstance(other,Value) else Value(other)
+        out=Value(self.data-other.data,_children=(self,other),_op='-')
+        def _backward():
+            self.grad+=1.0*out.grad
+            other.grad+=-1.0*out.grad
+        out._backward=_backward
+        return out
+    
+    def __truediv__(self,other):
+        return self*(other**-1)
+    
+    def tanh(self):
+        x=self.data
+        t=(math.exp(2*x)-1)/(math.exp(2*x)+1)
+        out=Value(t,_children=(self,),_op='tanh')
+        def _backward():
+            self.grad+=(1-t**2)*out.grad
+        out._backward=_backward
+        return out
+    
+    def exp(self):
+        x=self.data
+        out=Value(math.exp(x),_children=(self,),_op='exp')
+        def _backward():
+            self.grad+=out.data*out.grad
+        out._backward=_backward
+        return out
+    
+    def backward(self):
+        topo=[]
+        visited=set()
+        def build_topo(v): 
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad=1.0
+        for node in reversed(topo): #reversed beacause we want to go from output to input
+            node._backward()
+    
+    
+    
